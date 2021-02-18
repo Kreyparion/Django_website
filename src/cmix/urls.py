dae4753444b9bafed383aaa45133e279adbc271d
Maass Martinez Javier Esteban <javier.maass@student-cs.fr>
2021-02-18 21:08:38 +0100

    Solved Forms Added Prestas

--- src/cmix/urls.py
+++ src/cmix/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
     path('forms/', include('forms.urls')),
+    path('presta/', include('presta.urls')),
 ]
---
+++ src/presta/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PrestaConfig(AppConfig):
+    name = 'presta'
